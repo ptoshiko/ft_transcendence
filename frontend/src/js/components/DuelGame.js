79cdd098fb03c8cb36a5709cb2731e185f2cd257
Evgenii Grigorian <evgenii.grigorian@tabby.ai>
2024-04-28 22:11:17 +0200

    front in progress

--- frontend/src/js/components/DuelGame.js
+++ frontend/src/js/components/DuelGame.js
@@ -1,10 +1,39 @@
+import {getGameByID, joinGame} from "../service/game.js";
+import {getMe, getUserByDisplayName} from "../service/users.js";
+import {formatAvatar} from "../helpers.js";
+
 export default class extends HTMLElement {
     constructor() {
         super();
     }
 
-    connectedCallback() {
+    async connectedCallback() {
+        this.gameID = this.getAttribute("game_id");
+        try {
+            const resp = await joinGame(this.gameID);
+        } catch (status) {
+            this.renderErrorPage();
+            return;
+        }
+
+        this.me = await getMe();
+        this.game = await getGameByID(this.gameID);
+
+        if (this.game.player1.id === this.me.id) {
+            this.opponent = await getUserByDisplayName(this.game.player2.id);
+        } else {
+            this.opponent = await getUserByDisplayName(this.game.player1.id);
+        }
+
         this.render();
+
+        if (this.game.player1.id === this.me.id) {
+            this.leftAvatar.src = formatAvatar(this.me.avatar);
+            this.rightAvatar.src = formatAvatar(this.opponent.avatar);
+        } else {
+            this.leftAvatar.src = formatAvatar(this.opponent.avatar);
+            this.rightAvatar.src = formatAvatar(this.me.avatar);
+        }
 
         document.title = "Game";
     }
@@ -20,8 +49,8 @@ export default class extends HTMLElement {
             <div class="temp-bg">
                 <div class="temp-text text-light">Waiting Opponent...</div>
             </div>
-            <img class="avatar left rounded-circle" src="kanye.webp" alt="avatar">
-            <img class="avatar right rounded-circle" src="404.jpeg" alt="avatar">
+            <img id="duel-left-avatar" class="avatar left rounded-circle" src="${formatAvatar(this.me.avatar)}" alt="avatar">
+            <img id="duel-right-avatar" class="avatar right rounded-circle" src="404.jpeg" alt="avatar">
             <div class="score">
                 <div id="player-score">0</div>
                 <div id="computer-score">0</div>
@@ -31,5 +60,17 @@ export default class extends HTMLElement {
             <div class="paddle right bg-danger" id="computer-paddle"></div>
         </div>
         `;
+
+        this.leftAvatar = this.querySelector("#duel-left-avatar");
+        this.rightAvatar = this.querySelector("#duel-right-avatar");
+    }
+
+    renderErrorPage() {
+        this.innerHTML = `
+        <tr-nav></tr-nav>
+        <div class="d-flex justify-content-center align-items-center" style="height: calc(100vh - 56px);">
+            <h1 style="font-size: 8vw" class="text-danger">No Games Today️🙁</h1>
+        </div>
+       `
     }
 }
--- frontend/src/js/service/game.js
+++ frontend/src/js/service/game.js
@@ -1,7 +1,8 @@
 import {API_ADDRESS} from "../constants.js";
 import {withAuthorizationHeader, withJSONContent} from "../middleware.js";
 
-const CREATE_GAME_API = API_ADDRESS+"/game/create/"
+const CREATE_GAME_API = API_ADDRESS+"/game/create/";
+const JOIN_GAME_API = API_ADDRESS+"/game/join/"
 
 export async function createGame(opponentID) {
     const accessToken = localStorage.getItem("access-token") || "";
@@ -23,4 +24,33 @@ export async function createGame(opponentID) {
 
 
     return await resp.json();
+}
+
+export async function joinGame(gameID) {
+    const accessToken = localStorage.getItem("access-token") || "";
+
+    let authHeader = {};
+    if (accessToken !== "") {
+        authHeader = withAuthorizationHeader({}, accessToken);
+    }
+
+    const req = {
+        "game_id": gameID,
+    }
+
+    const resp = await fetch(JOIN_GAME_API, {
+        method: "PUT",
+        headers: withJSONContent(authHeader),
+        body: JSON.stringify(req)
+    });
+
+    if (!resp.ok) {
+        throw resp.status;
+    }
+
+    return await resp.json();
+}
+
+export async function getGameByID(gameID) {
+
 }
