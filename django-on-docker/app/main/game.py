7e3755dacdddd72f21ccb0aef9269461aeb9646b
Your Angelina <angelina@gmail.com>
2024-04-30 15:51:55 +0000

    ball state

--- django-on-docker/app/main/game.py
+++ django-on-docker/app/main/game.py
@@ -1,15 +1,35 @@
 import asyncio
+import random
+import math
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from datetime import time
+
+class Direction:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
 
 class Paddle:
     def __init__(self, y):
         self.y = y
 
 class Ball:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.x = 50
+        self.y = 50
+        self.direction = Direction()
+        self.velocity = 0.025
+        while (abs(self.direction.x) <= 0.2 or abs(self.direction.y) >= 0.9):
+            heading = random.uniform(0, 2 * math.pi)
+            self.direction.x = math.cos(heading)
+            self.direction.y = math.sin(heading)
+
+    def update(self, delta):
+        self.x += self.direction.x * self.velocity * delta
+        self.y += self.direction.y * self.velocity * delta
+        if self.y <= 0 or self.y >= 100:
+            self.direction.y *= -1
 
 class Game:
     def __init__(self, game_id, player1_id, player2_id):
@@ -18,10 +38,12 @@ class Game:
         self.player2_id = player2_id
         self.paddle1 = Paddle(50)
         self.paddle2 = Paddle(50)
-        self.ball = Ball(50, 50)
+        self.ball = Ball()
         self.player1_score = 0
         self.player2_score = 0
         self.current_tick = 3
+        self.not_finished = False
+        self.last_time = None
         
     async def tick(self):
         await asyncio.sleep(1)
@@ -49,8 +71,35 @@ class Game:
     
     async def start_game(self):
         await self.start_tick()
-        
-        # start game
+
+        self.start = time.time()
+
+        while (self.not_finished):
+            channel_layer = get_channel_layer()
+            await asyncio.gather(channel_layer.group_send(
+                f"{self.player1_id}",
+                {
+                    'type': 'game_state', 
+                    'ball_x': self.ball.x,
+                    'ball_y': self.ball.y,
+                }
+            ), channel_layer.group_send(
+                f"{self.player2_id}",
+                {
+                    'type': 'game_state', 
+                    'ball_x': self.ball.x,
+                    'ball_y': self.ball.y,
+                }
+            ))
+            await self.update_game(time.time()-self.start)
+            await asyncio.sleep(0.02)
+    
+    async def update_game(self, tm):
+        if self.last_time is not None:
+            delta = tm - self.last_time
+            self.ball.update(delta)
+
+        self.last_time = tm     
 
 
 class GameManager:
